2552e1bfda663ff9b5784707813c4181c36c917b
WesleySherman <wesley.sherman@expsouthafrica.co.za>
2024-12-17 12:29:19 +0200

    initial commit

---
+++ data.js
@@ -0,0 +1,629 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---lobby",
+      "name": "01 - Lobby",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.7923315156532933,
+        "pitch": 0.02093877469531158,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3619417456990242,
+          "pitch": 0.08471488528496174,
+          "rotation": 0,
+          "target": "1-02---floor-area-01"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2595148770054632,
+          "pitch": -0.3411784802263522,
+          "title": "Available Unit",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-02---floor-area-01",
+      "name": "02 - Floor Area 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20334661581618185,
+          "pitch": 0.13779231182440377,
+          "rotation": 0,
+          "target": "0-01---lobby"
+        },
+        {
+          "yaw": -3.046430586389617,
+          "pitch": 0.09525787274007946,
+          "rotation": 0,
+          "target": "2-03---floor-area-02"
+        },
+        {
+          "yaw": -2.3744051192852247,
+          "pitch": 0.08102508534817865,
+          "rotation": 0,
+          "target": "3-04---floor-area-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---floor-area-02",
+      "name": "03 - Floor Area 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.004995070766732113,
+        "pitch": -0.006954045306134304,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.030705769685546613,
+          "pitch": 0.09572055357310738,
+          "rotation": 0,
+          "target": "1-02---floor-area-01"
+        },
+        {
+          "yaw": -1.2013577055487623,
+          "pitch": 0.15489470322827614,
+          "rotation": 0,
+          "target": "3-04---floor-area-03"
+        },
+        {
+          "yaw": -2.2766536529556003,
+          "pitch": 0.1299034562388499,
+          "rotation": 0,
+          "target": "4-05---floor-area-04"
+        },
+        {
+          "yaw": -2.977823565918552,
+          "pitch": 0.08432613894479246,
+          "rotation": 0,
+          "target": "7-08---male-bathrooms"
+        },
+        {
+          "yaw": -3.138484319141231,
+          "pitch": -0.01305962491160706,
+          "rotation": 4.71238898038469,
+          "target": "8-09---female-bathrooms-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04---floor-area-03",
+      "name": "04 - Floor Area 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.291023485012891,
+        "pitch": 0.015396950604007031,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1128760157418958,
+          "pitch": 0.110904666950713,
+          "rotation": 0,
+          "target": "2-03---floor-area-02"
+        },
+        {
+          "yaw": 1.049057353575705,
+          "pitch": 0.09500752714563276,
+          "rotation": 0,
+          "target": "1-02---floor-area-01"
+        },
+        {
+          "yaw": 3.13835562743507,
+          "pitch": 0.12596147207841923,
+          "rotation": 0,
+          "target": "4-05---floor-area-04"
+        },
+        {
+          "yaw": -1.9841829829120492,
+          "pitch": 0.09883885230155798,
+          "rotation": 0,
+          "target": "6-07---floor-area-06"
+        },
+        {
+          "yaw": -2.460868592449563,
+          "pitch": 0.08338609225466342,
+          "rotation": 0,
+          "target": "5-06---floor-area-05"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05---floor-area-04",
+      "name": "05 - Floor Area 04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 3.073082794741918,
+        "pitch": 0.02347173249672707,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0988599337549907,
+          "pitch": 0.11858872977523305,
+          "rotation": 0,
+          "target": "3-04---floor-area-03"
+        },
+        {
+          "yaw": -1.8479992236167782,
+          "pitch": 0.08687653742579116,
+          "rotation": 0,
+          "target": "2-03---floor-area-02"
+        },
+        {
+          "yaw": -2.5463373825980575,
+          "pitch": 0.05900610251810434,
+          "rotation": 0,
+          "target": "1-02---floor-area-01"
+        },
+        {
+          "yaw": 1.211708572665847,
+          "pitch": 0.12384194140480176,
+          "rotation": 0,
+          "target": "5-06---floor-area-05"
+        },
+        {
+          "yaw": 2.150083227111544,
+          "pitch": 0.12991723983997616,
+          "rotation": 0,
+          "target": "6-07---floor-area-06"
+        },
+        {
+          "yaw": -1.367647612960507,
+          "pitch": 0.02261590566548577,
+          "rotation": 0,
+          "target": "8-09---female-bathrooms-01"
+        },
+        {
+          "yaw": -1.2695537355845516,
+          "pitch": 0.021904449229673162,
+          "rotation": 1.5707963267948966,
+          "target": "7-08---male-bathrooms"
+        },
+        {
+          "yaw": 0.19813404759601383,
+          "pitch": 0.0005741378959971399,
+          "rotation": 0,
+          "target": "10-11---back-of-property"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06---floor-area-05",
+      "name": "06 - Floor Area 05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.398093886268029,
+        "pitch": -0.027816181224544323,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0946266414743224,
+          "pitch": 0.12937668156757098,
+          "rotation": 0,
+          "target": "4-05---floor-area-04"
+        },
+        {
+          "yaw": -2.735164149352576,
+          "pitch": 0.15187121151119953,
+          "rotation": 0,
+          "target": "6-07---floor-area-06"
+        },
+        {
+          "yaw": -1.8750843715760368,
+          "pitch": 0.09604656448692417,
+          "rotation": 0,
+          "target": "3-04---floor-area-03"
+        },
+        {
+          "yaw": 0.07479084914169931,
+          "pitch": 0.014512240562106982,
+          "rotation": 0,
+          "target": "10-11---back-of-property"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-07---floor-area-06",
+      "name": "07 - Floor Area 06",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.9896761320993264,
+        "pitch": 0.00842475412572874,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4619569637826046,
+          "pitch": 0.05768012968476377,
+          "rotation": 0,
+          "target": "1-02---floor-area-01"
+        },
+        {
+          "yaw": 2.2070443712795864,
+          "pitch": 0.07537233389883014,
+          "rotation": 0,
+          "target": "2-03---floor-area-02"
+        },
+        {
+          "yaw": 1.937074145917463,
+          "pitch": 0.1584007861002661,
+          "rotation": 0,
+          "target": "3-04---floor-area-03"
+        },
+        {
+          "yaw": 2.7085715007785467,
+          "pitch": 0.11519179886454012,
+          "rotation": 0,
+          "target": "4-05---floor-area-04"
+        },
+        {
+          "yaw": -2.7815557784829608,
+          "pitch": 0.165622724932458,
+          "rotation": 0,
+          "target": "5-06---floor-area-05"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-08---male-bathrooms",
+      "name": "08 - Male Bathrooms",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.4363329604799375,
+        "pitch": 0.09967464938796411,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.337519914842108,
+          "pitch": 0.16106961801716047,
+          "rotation": 0,
+          "target": "2-03---floor-area-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09---female-bathrooms-01",
+      "name": "09 - Female Bathrooms 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.22293997998162496,
+        "pitch": 0.05394046285603871,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4822899625820716,
+          "pitch": 0.4731554750707918,
+          "rotation": 5.497787143782138,
+          "target": "9-10---female-bathrooms-02"
+        },
+        {
+          "yaw": -2.5765790195608353,
+          "pitch": 0.30921661113158017,
+          "rotation": 1.5707963267948966,
+          "target": "2-03---floor-area-02"
+        },
+        {
+          "yaw": -2.766396455826838,
+          "pitch": 0.31517884916156014,
+          "rotation": 4.71238898038469,
+          "target": "7-08---male-bathrooms"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10---female-bathrooms-02",
+      "name": "10 - Female Bathrooms 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.0315190618412498,
+        "pitch": -0.008225778187146204,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.271093672899518,
+          "pitch": 0.1960923204695959,
+          "rotation": 0,
+          "target": "8-09---female-bathrooms-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-11---back-of-property",
+      "name": "11 - Back of Property",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.7267957845860273,
+        "pitch": -0.003371658330257432,
+        "fov": 1.352045691493442
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Warehouse 01, Walmer Downs Family Centre",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
